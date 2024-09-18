beeb93b9d608393ae450869658798fabd1d3d9b0
gununes031 <gununes31@yahoo.com.br>
2024-09-18 13:29:58 -0300

    desafio poo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia Cel1 = new Nokia("11 970594877", "Nokia 2.0", "111111024", "128 gb");
+
+Cel1.Ligar();
+Cel1.ReceberLigacao();
+Cel1.InstalarAplicativo("Clash of Clans");
+
+
+
+Iphone Cel2 = new Iphone("11 956063189", "15 Pro Max", "9564000011", "256 gb");
+
+Cel2.Ligar();
+Cel2.ReceberLigacao();
+Cel2.InstalarAplicativo("Instagram");
